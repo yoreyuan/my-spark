9bffe635267b3f1fe043d10430fcfce246f51592
yoreyuan <791072311@qq.com>
2019-03-12 16:34:05 +0800

    添加说明，从LinkedBlockingQueue取元素时优先使用take()

--- demo/business-practice/E-commerce_click_real-time_stream/src/main/java/yore/db/JDBCWrapper.java
+++ demo/business-practice/E-commerce_click_real-time_stream/src/main/java/yore/db/JDBCWrapper.java
@@ -9,10 +9,28 @@ import java.util.concurrent.LinkedBlockingQueue;
  */
 public class JDBCWrapper {
 
-    private static LinkedBlockingQueue<Connection> dbConnectionPool = new LinkedBlockingQueue<>();
+    static final Integer POOL_SIZE = 10;
+
+    /**
+     * 连接池队列
+     *
+     * java.util.concurrent包下的新类，是一个阻塞的线程安全的队列，底层采用链表实现。
+     * LinkedBlockingQueue构造的时候若没有指定大小，则默认大小为Integer.MAX_VALUE，当然也可以在构造函数的参数中指定大小。LinkedBlockingQueue不接受null。
+     *
+     * 添加元素：
+     *      add 方法在添加元素的时候，若超出了队列的长度会直接抛出异常：
+     *      put 方法，若向队尾添加元素的时候发现队列已经满了会发生阻塞一直等待空间，以加入元素。
+     *      offer 方法在添加元素时，如果发现队列已满无法添加的话，会直接返回false。
+     *
+     * 从队列中取出并移除头元素：
+     *      poll: 若队列为空，返回null。
+     *      remove:若队列为空，抛出NoSuchElementException异常。
+     *      take:若队列为空，发生阻塞，等待有元素。
+     */
+    private static LinkedBlockingQueue<Connection> dbConnectionPool = new LinkedBlockingQueue<>(POOL_SIZE);
 
     private JDBCWrapper(){
-        for(int i =0;i<10; i++){
+        for(int i =0;i<POOL_SIZE; i++){
             try {
                 Connection conn = DriverManager.getConnection(
                         "jdbc:mysql://cdh1:3306/sparkstreaming",
@@ -57,7 +75,12 @@ public class JDBCWrapper {
                 e.printStackTrace();
             }
         }
-        return dbConnectionPool.poll();
+        try {
+            return dbConnectionPool.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return dbConnectionPool.poll();
+        }
     }
 
     /**
